9d2aecca67012d9a81aaf9ae123d6bb4b338db8e
Yuri-Cruz <yuri.cruz@bandtec.com.br>
2022-05-22 15:36:31 -0300

    alteracao 2.0

--- src/components/PedidosOrcamentoTecnico.jsx
+++ src/components/PedidosOrcamentoTecnico.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import React, { useContext, useEffect, useState } from 'react'
 // import BarInformacaoCliente from "../BarInformacaoCliente";
-import BoxProdOrcamento from "../BoxProdOrcamento";
+// import BoxProdOrcamento from "../BoxProdOrcamento";
 import { useRouter } from "next/router";
 import Carregamento from '../Carregamento';
 import Erros from '../Erros';
@@ -89,7 +89,7 @@ function pedidosOrcamentosTecnico() {
                 {/* <BarInformacaoCliente id={pedido.id} status={pedido.statusGeral} nome={pedido.solicitante.nome} data={pedido.dataSolicitacao} /> */}
                 <div className="p-8 flex flex-col items-center justify-evenly border-2 border-gray-dark border-solid rounded-xl rounded-t-none shadow-lg">
                     <Erros erros={erros} />
-                    {pedido.itens.map((item, i) => <BoxProdOrcamento key={i} id={item.id} somaTotal={somaTotal} tipo={item.produto.tipo} marca={item.produto.marca} modelo={item.produto.modelo} problema={item.problema} descricao={item.descricao} />)}
+                    {/* {pedido.itens.map((item, i) => <BoxProdOrcamento key={i} id={item.id} somaTotal={somaTotal} tipo={item.produto.tipo} marca={item.produto.marca} modelo={item.produto.modelo} problema={item.problema} descricao={item.descricao} />)} */}
 
                     <div className="flex flex-col w-full items-end">
                         <div className="bg-blue-light_dark p-4 w-1/5 rounded-xl mb-1">
